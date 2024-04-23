70a28cdd117c0eb331378fe3f38e961317e8a46b
Anieelorez <anieelorez1@gmail.com>
2024-04-23 17:02:42 +0100

    modified nat-gateway.tf

--- nat-gateway.tf
+++ nat-gateway.tf
@@ -1,7 +1,7 @@
 resource "aws_eip" "eip_for_nat_gateway_az1" {
   domain = "vpc"
 
-  tags   = {
+  tags = {
     Name = "nat_gateway_az1_eip"
   }
 }
@@ -9,7 +9,7 @@ resource "aws_eip" "eip_for_nat_gateway_az1" {
 resource "aws_eip" "eip_for_nat_gateway_az2" {
   domain = "vpc"
 
-  tags   = {
+  tags = {
     Name = "nat_gateway_az2_eip"
   }
 }
@@ -19,20 +19,20 @@ resource "aws_nat_gateway" "nat_gateway_az1" {
   allocation_id = aws_eip.eip_for_nat_gateway_az1.id
   subnet_id     = aws_subnet.public_subnet_az1.id
 
-  tags   = {
+  tags = {
     Name = "nat_gateway_az1"
   }
 
-  depends_on = [ aws_internet_gateway.internet_gateway ]
+  depends_on = [aws_internet_gateway.internet_gateway]
 }
 
 resource "aws_nat_gateway" "nat_gateway_az2" {
   allocation_id = aws_eip.eip_for_nat_gateway_az2.id
   subnet_id     = aws_subnet.public_subnet_az2.id
 
-  tags   = {
+  tags = {
     Name = "nat_gateway_az2"
-   
+
   }
 
   depends_on = [aws_internet_gateway.internet_gateway]
@@ -41,40 +41,40 @@ resource "aws_nat_gateway" "nat_gateway_az2" {
 
 # create private route table az1 and add route through nat gateway az1
 resource "aws_route_table" "private_route_table_az1" {
-  vpc_id            = aws_vpc.eks-vpc.id
+  vpc_id = aws_vpc.eks-vpc.id
 
   route {
-    cidr_block      = "0.0.0.0/0"
-    nat_gateway_id  = aws_nat_gateway.nat_gateway_az1.id  
-    }
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway_az1.id
+  }
 
-  tags   = {
+  tags = {
     Name = "private route table az1"
   }
 }
 
 # associate private subnet az1 to "private route table"
 resource "aws_route_table_association" "private_subnet_az1_route_table_association" {
-  subnet_id           = aws_subnet.private_subnet_az1.id
-  route_table_id      = aws_route_table.private_route_table_az1.id
+  subnet_id      = aws_subnet.private_subnet_az1.id
+  route_table_id = aws_route_table.private_route_table_az1.id
 }
 
-# create private route table az1 and add route through nat gateway az1
+# create private route table az2 and add route through nat gateway az1
 resource "aws_route_table" "private_route_table_az2" {
-  vpc_id            = aws_vpc.eks-vpc.id
+  vpc_id = aws_vpc.eks-vpc.id
 
   route {
-    cidr_block      = "0.0.0.0/0"
-    nat_gateway_id  = aws_nat_gateway.nat_gateway_az2.id  
-    }
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway_az2.id
+  }
 
-  tags   = {
-    Name = "private route table az1"
+  tags = {
+    Name = "private route table az2"
   }
 }
 
 # associate public subnet az2 to "private route table"
 resource "aws_route_table_association" "private_subnet_az2_route_table_association" {
-  subnet_id           = aws_subnet.private_subnet_az2.id
-  route_table_id      = aws_route_table.private_route_table_az2.id
+  subnet_id      = aws_subnet.private_subnet_az2.id
+  route_table_id = aws_route_table.private_route_table_az2.id
 }
