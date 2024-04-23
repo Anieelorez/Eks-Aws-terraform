cb45e5304c957cc46d3bf0a67ac112c3b77ad8a6
Anieelorez <anieelorez1@gmail.com>
2024-04-23 19:24:24 +0100

    modified vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc" "eks-vpc" {
-  cidr_block       = var.vpc_cidr
-  instance_tenancy = "default"
+  cidr_block           = var.vpc_cidr
+  instance_tenancy     = "default"
   enable_dns_hostnames = true
 
   tags = {
@@ -9,17 +9,17 @@ resource "aws_vpc" "eks-vpc" {
 }
 
 resource "aws_internet_gateway" "internet_gateway" {
-  vpc_id    = aws_vpc.eks-vpc.id
+  vpc_id = aws_vpc.eks-vpc.id
 
-  tags      = {
-    Name    = "Eks-igw"
+  tags = {
+    Name = "Eks-igw"
   }
 }
 
 resource "aws_subnet" "public_subnet_az1" {
-  vpc_id     = aws_vpc.eks-vpc.id
-  cidr_block = var.public_subnet_az1_cidr
-  availability_zone = "us-east-1a"
+  vpc_id                  = aws_vpc.eks-vpc.id
+  cidr_block              = var.public_subnet_az1_cidr
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 
   tags = {
@@ -28,9 +28,9 @@ resource "aws_subnet" "public_subnet_az1" {
 }
 
 resource "aws_subnet" "public_subnet_az2" {
-  vpc_id     = aws_vpc.eks-vpc.id
-  cidr_block = var.public_subnet_az2_cidr
-  availability_zone = "us-east-1b"
+  vpc_id                  = aws_vpc.eks-vpc.id
+  cidr_block              = var.public_subnet_az2_cidr
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 
   tags = {
@@ -39,48 +39,48 @@ resource "aws_subnet" "public_subnet_az2" {
 }
 
 resource "aws_route_table" "public_route_table" {
-  vpc_id       = aws_vpc.eks-vpc.id
+  vpc_id = aws_vpc.eks-vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.internet_gateway.id
   }
 
-  tags       = {
-    Name     = "public-route-table"
+  tags = {
+    Name = "public-route-table"
   }
 }
 
 # associate public subnet az1 to "public route table"
 resource "aws_route_table_association" "public_subnet_az1_route_table_association" {
-  subnet_id           = aws_subnet.public_subnet_az1.id
-  route_table_id      = aws_route_table.public_route_table.id
+  subnet_id      = aws_subnet.public_subnet_az1.id
+  route_table_id = aws_route_table.public_route_table.id
 }
 
 # associate public subnet az2 to "public route table"
 resource "aws_route_table_association" "public_subnet_az2_route_table_association" {
-  subnet_id           = aws_subnet.public_subnet_az2.id
-  route_table_id      = aws_route_table.public_route_table.id
+  subnet_id      = aws_subnet.public_subnet_az2.id
+  route_table_id = aws_route_table.public_route_table.id
 }
 
 resource "aws_subnet" "private_subnet_az1" {
-  vpc_id     = aws_vpc.eks-vpc.id
-  cidr_block = var.private_subnet_az1_cidr
-  availability_zone = "us-east-1a"
+  vpc_id                  = aws_vpc.eks-vpc.id
+  cidr_block              = var.private_subnet_az1_cidr
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = false
 
   tags = {
-    Name = "private_app_subnet_az1"
+    Name = "private_subnet_az1"
   }
 }
 
 resource "aws_subnet" "private_subnet_az2" {
-  vpc_id     = aws_vpc.eks-vpc.id
-  cidr_block = var.private_subnet_az2_cidr
-  availability_zone = "us-east-1b"
+  vpc_id                  = aws_vpc.eks-vpc.id
+  cidr_block              = var.private_subnet_az2_cidr
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = false
 
   tags = {
-    Name = "private_app_subnet_az2"
+    Name = "private_subnet_az2"
   }
 }
